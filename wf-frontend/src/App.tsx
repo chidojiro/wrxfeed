8b09eea35e3842313b073e86d789ecf781d66157
matt <mlock23@gmail.com>
2021-09-19 16:41:51 -0400

    added tailwind

--- wf-frontend/src/App.tsx
+++ wf-frontend/src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import Dashboard from './components/routes/Dashboard';
+import {
+  DASHBOARD
+} from './constants/routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <Router>
+        <Switch>
+          <Route path={DASHBOARD} exact>
+            <Dashboard />
+          </Route>
+        </Switch>
+      </Router>
+     
+
   );
 }
 
---
+++ wf-frontend/src/components/routes/Dashboard.tsx
@@ -0,0 +1,11 @@
+
+
+function Dashboard() {
+  return (
+    <div className='px-4 lg:px-12 xl:px-24 max-w-7xl mx-auto'>
+
+    </div>
+  )
+}
+
+export default Dashboard;
