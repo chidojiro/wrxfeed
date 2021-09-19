b54ba28a230b9eada72bdaffd225e2c8eb5eba3d
matt <mlock23@gmail.com>
2021-09-19 18:51:47 -0400

    added more to overview

--- wf-frontend/src/components/layout/Overview.tsx
+++ wf-frontend/src/components/layout/Overview.tsx
@@ -4,6 +4,7 @@ import { createUseStyles } from 'react-jss';
 const useStyles = createUseStyles({
   container: {
     // display: 'flex',
+    width: '80%',
   },
   heading: {
 
@@ -13,6 +14,25 @@ const useStyles = createUseStyles({
   },
   lineItem: {
 
+
+  },
+  lineItemTitle: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  lineItemInfo: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  lineItemDate: {
+    paddingLeft: 18,
+  },
+  lineItemAmount: {
+
+  },
+  lineItemDetail: {
+    
   }
 });
 
@@ -27,6 +47,18 @@ function Overview(){
         <h3>Company transactions</h3>
       </div>
       <div className={classes.lineItem}>
+        <div className={classes.lineItemTitle}>
+          <div className={classes.lineItemInfo}>
+        <h4>Prfessional Services</h4>
+        <text className={classes.lineItemDate}>9/14/2021</text>
+        </div>
+        <div className={classes.lineItemAmount}>
+        <h4>-$10,326.80</h4>
+        </div>
+        </div>
+        <div className={classes.lineItemDetail}>
+          <text>Law offices of Lock Miller Schnell</text>
+        </div>
 
 
       </div>
