0e6bc10b116de4b42dd2a987d3601384c2b36c3c
An Tran <tienan92it@gmail.com>
2021-09-22 17:50:38 +0700

    feat: sign In with google

---
+++ src/api/containers/AuthProvider.tsx
@@ -0,0 +1,100 @@
+import React, { useContext, useState, useEffect } from 'react';
+import AuthStateContext from '@api/contexts/AuthContext';
+import { User } from 'firebase/auth';
+import { Identity } from '@/identity';
+
+interface AuthStateProviderProps {
+  Firebase: any;
+}
+
+const AuthStateProvider: React.FC<AuthStateProviderProps> = ({ children, Firebase }) => {
+  const [authState, setAuthState] = useState<Identity>();
+
+  const signOut = () => Firebase.signOut(setAuthState);
+  const signUpWithEmailAndPassword = (
+    email: string,
+    password: string,
+    name: string,
+    callback: (user: User | null | undefined) => void,
+    onError: (error: any) => void,
+  ) => {
+    const expandedOnError = (error: any) => {
+      // If there is an error with the login we will rollback to the last auth state.
+      setAuthState(authState);
+      onError(error);
+    };
+    Firebase.signUpWithEmailAndPassword(
+      email,
+      password,
+      name,
+      setAuthState,
+      callback,
+      expandedOnError,
+    );
+  };
+
+  const signInWithEmailAndPassword = (
+    email: string,
+    password: string,
+    callback: (user: User | null | undefined) => void,
+    onError: (error: any) => void,
+  ) => {
+    const expandedOnError = (error: any) => {
+      // If there is an error with the login we will rollback to the last auth state.
+      setAuthState(authState);
+      onError(error);
+    };
+    Firebase.signInWithEmailAndPassword(email, password, setAuthState, callback, expandedOnError);
+  };
+  const signInWithFacebook = (
+    callback: (user: User | null | undefined) => void,
+    onError: (error: any) => void,
+  ) => Firebase.signInWithFacebook(setAuthState, callback, onError);
+
+  const signInWithGoogle = (callback: (user: User | null | undefined) => void) =>
+    Firebase.signInWithGoogle(setAuthState, callback);
+
+  const updateEmailAddress = (email: string, callback: (user: User | null | undefined) => void) =>
+    Firebase.updateEmailAddress(email, callback);
+
+  const sendPasswordResetEmail = (
+    email: string,
+    callback: (user: User | null | undefined) => void,
+    onError: (error: any) => void,
+  ) => Firebase.sendPasswordResetEmail(email, callback, onError);
+
+  const sendEmailVerification = (
+    callback: (user: User | null | undefined) => void,
+    onError: (error: any) => void,
+  ) => Firebase.sendEmailVerification(callback, onError);
+
+  const userHasOnlyEmailProvider = () => Firebase.userHasOnlyEmailProvider();
+  useEffect(() => {
+    const unsubscribe = Firebase.authState(setAuthState);
+    return () => unsubscribe();
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <AuthStateContext.Provider
+      value={{
+        identity: authState,
+        signOut,
+        signUpWithEmailAndPassword,
+        signInWithEmailAndPassword,
+        signInWithFacebook,
+        signInWithGoogle,
+        updateEmailAddress,
+        sendPasswordResetEmail,
+        sendEmailVerification,
+        userHasOnlyEmailProvider,
+      }}
+    >
+      {children}
+    </AuthStateContext.Provider>
+  );
+};
+
+export default AuthStateProvider;
+
+export const useAuthStateContext = () => useContext(AuthStateContext);
--- src/identity/index.ts
+++ src/identity/index.ts
@@ -1,5 +1,6 @@
 export { useIdentity, useSetIdentity } from './hooks';
 export { identityState } from './states';
 export { default as withAuth } from './hocs';
+// eslint-disable-next-line import/no-cycle
 export { default as ProtectedRoute } from './atoms/ProtectedRoute';
 export type { Identity } from './types';
