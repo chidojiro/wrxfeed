81ad8f0574696309671b35db8cbf40785160d701
An Tran <tienan92it@gmail.com>
2021-10-01 22:53:33 +0700

    feat: handle transaction load more

--- src/main/hooks/transaction.hook.ts
+++ src/main/hooks/transaction.hook.ts
@@ -1,17 +1,43 @@
+import { useApi } from '@api';
+import { Pagination } from '@api/types';
+import { useErrorHandler } from '@error/hooks';
+import { isBadRequest } from '@error/utils';
 import { Transaction } from '@main/entity';
-import { transactionFilterState, transactionSelector } from '@main/states/transaction.state';
-import { useCallback, useEffect } from 'react';
-import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { useCallback, useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 
 interface TransactionHookValues {
   transactions: Transaction[];
+  hasMore: boolean;
+  isLoading: boolean;
 }
 
-export function useTransaction(): TransactionHookValues {
-  const result = useRecoilValue(transactionSelector);
-  const setRefreshFlag = useSetRecoilState(transactionFilterState);
-  const refresh = useCallback(() => setRefreshFlag((flag) => ({ ...flag })), [setRefreshFlag]);
-  // invalidate list when component is unmounted
-  useEffect(() => refresh, [refresh]);
-  return { transactions: result };
+export function useTransaction(pagination: Pagination): TransactionHookValues {
+  const [transactions, setTransaction] = useState<Transaction[]>([]);
+  const [hasMore, setHasMore] = useState<boolean>(false);
+  const [isLoading, setLoading] = useState<boolean>(false);
+  const ApiClient = useApi();
+  const errorHandler = useErrorHandler();
+
+  const getTransactions = useCallback(async () => {
+    try {
+      setLoading(true);
+      const res = await ApiClient.getTransactions(pagination);
+      setTransaction((prevTrans) => [...prevTrans, ...res]);
+      setHasMore(!!res.length);
+    } catch (error) {
+      if (isBadRequest(error)) {
+        toast.error('Can not get transactions');
+      } else {
+        await errorHandler(error);
+      }
+    } finally {
+      setLoading(false);
+    }
+  }, [ApiClient, errorHandler, pagination]);
+
+  useEffect(() => {
+    getTransactions().then();
+  }, [getTransactions]);
+  return { transactions, hasMore, isLoading };
 }
--- src/main/organisms/TransactionList/index.tsx
+++ src/main/organisms/TransactionList/index.tsx
@@ -1,20 +1,59 @@
-import React from 'react';
-
-import Divider from '@mui/material/Divider';
+import React, { useCallback, useState } from 'react';
+import { Box, Divider, Skeleton, Stack } from '@mui/material';
 import TransactionItem from '@main/molecules/TransactionItem';
 import { useTransaction } from '@main/hooks';
+// import InfiniteScroll from 'react-infinite-scroller';
+import { Pagination } from '@api/types';
+import InfiniteScroller from '@common/atoms/InfiniteScroller';
+
+const LIMIT = 10;
 
 const TransactionList: React.VFC = () => {
-  const { transactions = [] } = useTransaction();
+  const [filter, setFilter] = useState<Pagination>({ offset: 0, limit: LIMIT });
+  const { transactions, hasMore, isLoading } = useTransaction(filter);
+
+  const handleLoadMore = useCallback(() => {
+    if (!hasMore || isLoading) return;
+    setFilter((prevFilter) => ({
+      ...prevFilter,
+      offset: prevFilter.offset + prevFilter.limit,
+    }));
+  }, [hasMore, isLoading]);
+
+  const renderLoadingSkeleton = () => (
+    <Stack sx={{ ml: 1 }} direction="row">
+      <Box sx={{ margin: 1 }}>
+        <Skeleton variant="circular" width={32} height={32} />
+      </Box>
+      <Box sx={{ width: '100%', mr: 3, mt: 1, ml: 1 }}>
+        <Skeleton sx={{ borderRadius: 1 }} animation="wave" width="100%" />
+        <Skeleton sx={{ borderRadius: 1, mt: 3 }} variant="rectangular" width="100%" height={50} />
+      </Box>
+    </Stack>
+  );
+
   return (
-    <>
+    <InfiniteScroller
+      sx={{
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        overflow: 'auto',
+        paddingRight: 2,
+      }}
+      onLoadMore={handleLoadMore}
+      isLoading={isLoading}
+      LoadingComponent={renderLoadingSkeleton()}
+    >
       {transactions.map((transaction) => (
         <React.Fragment key={transaction.id}>
           <Divider />
           <TransactionItem transaction={transaction} />
         </React.Fragment>
       ))}
-    </>
+    </InfiniteScroller>
   );
 };
 
