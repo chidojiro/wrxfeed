a069a4de50deef4c96bfd1925ce6da36637d8f8e
An Tran <tienan92it@gmail.com>
2021-10-04 18:42:38 +0700

    feat: handle load more comment and improve transaction lazy load with debounce

--- src/main/hooks/comment.hook.ts
+++ src/main/hooks/comment.hook.ts
@@ -3,42 +3,51 @@ import { OrderDirection, Pagination } from '@api/types';
 import { useErrorHandler } from '@error/hooks';
 import { isBadRequest } from '@error/utils';
 import { useIdentity } from '@identity/hooks';
-import { Comment } from '@main/entity';
+import { Comment, Transaction } from '@main/entity';
 import { CommentFormModel } from '@main/types';
 import { useCallback, useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 
 interface CommentHookValues {
   comments: Comment[];
+  total: number;
+  isLoading: boolean;
   addComment: (comment: CommentFormModel) => Promise<void>;
 }
 
-export function useComment(transactionId: number, pagination?: Pagination): CommentHookValues {
+export function useComment(transaction: Transaction, pagination?: Pagination): CommentHookValues {
   const identity = useIdentity();
   const ApiClient = useApi();
   const errorHandler = useErrorHandler();
   const [comments, setComments] = useState<Comment[]>([]);
+  const [total, setTotal] = useState(transaction.commentCount ?? 0);
+  const [isLoading, setLoading] = useState(false);
 
   const getComments = useCallback(async () => {
     try {
+      setLoading(true);
       const res = await ApiClient.getComments({
-        transactionId,
+        transactionId: transaction.id,
         order: OrderDirection.DESC,
         pagination,
       });
-      setComments((prevComments) => [...prevComments, ...res.reverse()]);
+      // Comments're shown from bottom to top => need to reverse it;
+      const reverse = res.reverse();
+      setComments((prevComments) => [...reverse, ...prevComments]);
     } catch (error) {
       if (isBadRequest(error)) {
         toast.error('Can not get comments');
       } else {
         await errorHandler(error);
       }
+    } finally {
+      setLoading(false);
     }
-  }, [ApiClient, errorHandler, transactionId, pagination]);
+  }, [ApiClient, errorHandler, transaction.id, pagination]);
 
   const addComment = async (comment: CommentFormModel) => {
     try {
-      const res = await ApiClient.addComment(transactionId, comment);
+      const res = await ApiClient.addComment(transaction.id, comment);
       if (!res.user) {
         res.user = {
           email: identity?.email || '',
@@ -46,6 +55,7 @@ export function useComment(transactionId: number, pagination?: Pagination): Comm
         };
       }
       setComments((prevComments) => [...prevComments, res]);
+      setTotal(total + 1);
     } catch (error) {
       if (isBadRequest(error)) {
         toast.error('Can not get comments');
@@ -60,5 +70,5 @@ export function useComment(transactionId: number, pagination?: Pagination): Comm
     getComments().then();
   }, [getComments]);
 
-  return { comments, addComment };
+  return { comments, total, isLoading, addComment };
 }
