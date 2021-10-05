6d1dbdfc5bf1bb9cadabd7521c0d8230fcc213fc
An Tran <tienan92it@gmail.com>
2021-10-05 17:32:20 +0700

    feat: convert event emitter to ts

--- src/main/EventEmitter.ts
+++ src/main/EventEmitter.ts
@@ -1,21 +1,30 @@
-// export default {
-//   _events: {},
-//   dispatch(event, data) {
-//     if (!this._events[event]) return;
-//     this._events[event].forEach((callback) => callback(data));
-//   },
-//   subscribe(event, callback) {
-//     if (typeof callback !== 'function') {
-//       throw Error(`${event}: Event Callback must be a function`);
-//     }
-//     if (!this._events[event]) this._events[event] = [];
-//     this._events[event].push(callback);
-//   },
-//   unsubscribe(event, callback) {
-//     if (typeof callback !== 'function') {
-//       throw Error(`${event}: Event Callback must be a function`);
-//     }
-//     if (!this._events[event]) return;
-//     this._events[event] = this._events[event].filter((cb) => cb !== callback);
-//   },
-// };
+export enum EventName {
+  EXAMPLE = 'EXAMPLE',
+}
+
+type Callback = (...args: unknown[]) => unknown;
+interface EventObj {
+  [index: string]: Callback[];
+}
+
+export default {
+  events: <EventObj>{},
+  dispatch(event: EventName, data: unknown): void {
+    if (!this.events[event]) return;
+    this.events[event].forEach((callback) => callback(data));
+  },
+  subscribe(event: EventName, callback: Callback): void {
+    if (typeof callback !== 'function') {
+      throw Error(`${event}: Event Callback must be a function`);
+    }
+    if (!this.events[event]) this.events[event] = [];
+    this.events[event].push(callback);
+  },
+  unsubscribe(event: EventName, callback: Callback): void {
+    if (typeof callback !== 'function') {
+      throw Error(`${event}: Event Callback must be a function`);
+    }
+    if (!this.events[event]) return;
+    this.events[event] = this.events[event].filter((cb) => cb !== callback);
+  },
+};
