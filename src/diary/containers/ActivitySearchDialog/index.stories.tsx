1c7795e596399745f68596bf42eab26833fbed10
An Tran <tienan92it@gmail.com>
2021-10-11 17:10:36 +0700

    feat: remove redundant code

--- src/diary/containers/ActivitySearchDialog/index.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Meta } from '@storybook/react/types-6-0';
-import ActivitySearchDialog from '.';
-
-export default {
-  title: 'Containers/ActivitySearchDialog',
-  component: ActivitySearchDialog,
-} as Meta;
-
-export const Default = ActivitySearchDialog;
