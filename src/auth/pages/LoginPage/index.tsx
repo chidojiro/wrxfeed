0dcfd650303fcc31dc184deffdbc39a7e4fcc929
An Tran <tienan92it@gmail.com>
2021-10-27 01:44:53 +0700

    feat: ap-307 sign in from invitation

--- src/auth/pages/LoginPage/index.tsx
+++ src/auth/pages/LoginPage/index.tsx
@@ -1,6 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useEffect, useState } from 'react';
 import { useNavUtils } from '@common/hooks';
-import BlankLayout from '@common/templates/BlankLayout';
 import SocialAuthButton, { AuthProvider } from '@common/atoms/SocialAuthButton';
 import WrxfeedStar from '@auth/atoms/WrxfeedStar';
 import { GOOGLE_CLIENT_ID, GOOGLE_SCOPES } from '@src/config';
@@ -12,14 +12,30 @@ import { toast } from 'react-toastify';
 import { ProviderName } from '@main/entity';
 import { useErrorHandler, isApiError, ApiErrorCode } from '@src/error';
 import NotInvited from '@auth/molecules/NotInvited';
+import { useLocation } from 'react-router-dom';
+import NotifyBanner from '@common/molecules/NotifyBanner';
+import { LocationState } from '../AcceptInvitation';
 
 const LoginPage: React.VFC = () => {
   const { signInWithGoogle, getProfile } = useApi();
   const { redirect } = useNavUtils();
+  const location = useLocation();
   const identity = useIdentity();
   const setIdentity = useSetIdentity();
   const errorHandler = useErrorHandler();
   const [notInvited, setNotInvited] = useState(false);
+  // Variables
+  const { fromInvite, message } = location.state as LocationState;
+
+  useEffect(() => {
+    if (message) {
+      NotifyBanner.info(message, {
+        timeout: 3000,
+        topOffset: 0,
+        backgroundColor: '#374151',
+      });
+    }
+  }, [message]);
 
   useEffect(() => {
     if (identity?.token && identity?.lastLoginAt) {
@@ -77,31 +93,35 @@ const LoginPage: React.VFC = () => {
   return notInvited ? (
     <NotInvited />
   ) : (
-    <BlankLayout>
-      <div className="flex flex-col justify-center items-center mt-[6vh] space-y-10">
+    <div className="flex flex-col justify-center items-center min-h-screen my-auto space-y-10">
+      {fromInvite ? (
+        <h2 className="text-4xl text-primary text-center font-bold mb-3">
+          Join your team on Gravity.
+        </h2>
+      ) : (
         <div className="flex flex-col justify-center items-center">
           <WrxfeedStar />
           <p className="text-[87px] font-bold whitespace-pre-line text-center -mt-6 leading-[90px]">
             {'Reach your\nfinancial goal↗'}
           </p>
         </div>
-        <GoogleLogin
-          clientId={GOOGLE_CLIENT_ID}
-          scope={GOOGLE_SCOPES}
-          onSuccess={handleResponseSuccess}
-          onFailure={handleResponseFailure}
-          render={(renderProps) => (
-            <SocialAuthButton
-              provider={AuthProvider.GOOGLE}
-              disabled={renderProps.disabled}
-              onClick={renderProps.onClick}
-            >
-              Sign up with Google
-            </SocialAuthButton>
-          )}
-        />
-      </div>
-    </BlankLayout>
+      )}
+      <GoogleLogin
+        clientId={GOOGLE_CLIENT_ID}
+        scope={GOOGLE_SCOPES}
+        onSuccess={handleResponseSuccess}
+        onFailure={handleResponseFailure}
+        render={(renderProps) => (
+          <SocialAuthButton
+            provider={AuthProvider.GOOGLE}
+            disabled={renderProps.disabled}
+            onClick={renderProps.onClick}
+          >
+            Sign up with Google
+          </SocialAuthButton>
+        )}
+      />
+    </div>
   );
 };
 
