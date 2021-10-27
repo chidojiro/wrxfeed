a5de09bd1d57305d3869a63a088a8d0e378aa0b6
TaiPham <taipham1803@gmail.com>
2021-10-27 23:17:46 +0700

    fix: style notify popup, show createAt time

--- src/main/atoms/CommentText/index.tsx
+++ src/main/atoms/CommentText/index.tsx
@@ -1,16 +1,26 @@
 import TokenizedText from '@common/atoms/TokenizedText';
+import { classNames } from '@common/utils';
 import { tokenizeComment } from '@main/utils';
 import React from 'react';
 
 export interface CommentTextProps {
   content: string;
   showTagSymbol?: boolean;
+  className?: string;
+  useWhitespacePreWrap?: boolean;
 }
 
-const CommentText: React.VFC<CommentTextProps> = ({ content }) => {
+const CommentText: React.VFC<CommentTextProps> = ({
+  content,
+  className = '',
+  useWhitespacePreWrap = true,
+}) => {
   const tokenizedContent = tokenizeComment(content);
+  const wrapStyle = useWhitespacePreWrap ? 'whitespace-pre-wrap' : '';
   return (
-    <TokenizedText className="whitespace-pre-wrap break-words">{tokenizedContent}</TokenizedText>
+    <TokenizedText className={classNames('break-words', wrapStyle, className)}>
+      {tokenizedContent}
+    </TokenizedText>
   );
 };
 
--- src/main/atoms/NotifyRow/index.tsx
+++ src/main/atoms/NotifyRow/index.tsx
@@ -1,6 +1,6 @@
 import { Notification } from '@main/entity';
 import React from 'react';
-// import { formatDate } from '@common/utils';
+import { formatDate } from '@common/utils';
 import CommentText from '@main/atoms/CommentText';
 
 export interface NotifyRowProps {
@@ -11,39 +11,20 @@ export interface NotifyRowProps {
 }
 
 const NotifyRow: React.VFC<NotifyRowProps> = ({ item, onClickNotifyAndSeen }) => {
-  // const renderTransactionName = () => {
-  //   if (item?.transaction?.category) {
-  //     return (
-  //       <p
-  //         className="flex text-xs text-gray-1 font-bold truncate ..."
-  //         style={{ height: '24px', width: '120px' }}
-  //       >
-  //         {item?.transaction?.category?.name}
-  //       </p>
-  //     );
-  //   }
-  //   return <div className="flex text-xs text-gray-1">a transaction</div>;
-  // };
-
   return (
     <button
       type="button"
       onClick={onClickNotifyAndSeen}
       className="flex flex-col mt-2 min-h-16 px-4 py-2 hover:bg-Gray-hover"
     >
-      {/* <div className="flex flex-row items-center">
-        <div className="flex flex-row">
-          <div className="flex text-xs text-gray-1 font-bold">{item?.user?.fullName}</div>
-          <div className="flex text-xs text-gray-1 font-regular mx-1">mentioned you on</div>
-          {renderTransactionName()}
-        </div>
-        <div className="flex text-Gray-6 font-semibold ml-auto" style={{ fontSize: '10px' }}>
+      <div className="flex flex-1 w-full flex-row text-left">
+        <CommentText content={item.content} className="w-full" useWhitespacePreWrap={false} />
+        <div
+          className="flex text-Gray-6 font-semibold ml-auto w-32 text-right"
+          style={{ fontSize: '10px' }}
+        >
           {formatDate(item?.createdAt)}
         </div>
-      </div> */}
-      <div className="flex flex-row items-center text-left">
-        {/* <p className="flex text-xs text-Gray-2">{item.content}</p> */}
-        <CommentText content={item.content} />
       </div>
     </button>
   );
