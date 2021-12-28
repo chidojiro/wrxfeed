5770609769be13cf66cd11196bc2dd3a9f558008
TaiPham <taipham1803@gmail.com>
2021-12-28 08:43:26 +0700

    feat: add key enable show subscription in side bar in config

--- src/main/states/sidemenu.state.ts
+++ src/main/states/sidemenu.state.ts
@@ -3,6 +3,7 @@ import { MainGroups, MainMenu } from '@common/constants';
 import { GroupTab, LeftTab } from '@common/types';
 import { identityState } from '@identity/states';
 import { Category, Department, Subscription, Vendor } from '@main/entity';
+import { ENABLE_SUBSCRIPTION_SIDE_BAR } from '@src/config';
 import cloneDeep from 'lodash.clonedeep';
 import { atom, selector } from 'recoil';
 import { subscriptionState } from './subscription.state';
@@ -14,31 +15,33 @@ export interface FeedCount {
 export const menuItemsValue = selector<GroupTab[]>({
   key: 'main/sidemenu',
   get: ({ get }) => {
-    const subscription = get(subscriptionState);
-    const subscriptionMenuItems = Object.keys(subscription).reduce<LeftTab[]>(
-      (list, key) => [
-        ...list,
-        ...(subscription[key as keyof Subscription]?.map(
-          (channel: Department | Category | Vendor): LeftTab => ({
-            name: channel.name,
-            location: {
-              pathname: `/${key}/${channel.id}`,
-              search: `?route=${MainGroups.Feeds}`,
-            },
-            icon: null,
-            subscription: {
-              type: key as keyof Subscription,
-              item: channel,
-            },
-            removable: true,
-            strict: true,
-          }),
-        ) || []),
-      ],
-      [],
-    );
     const menu = cloneDeep(MainMenu);
-    menu[0].tabs.push(...subscriptionMenuItems);
+    if (ENABLE_SUBSCRIPTION_SIDE_BAR) {
+      const subscription = get(subscriptionState);
+      const subscriptionMenuItems = Object.keys(subscription).reduce<LeftTab[]>(
+        (list, key) => [
+          ...list,
+          ...(subscription[key as keyof Subscription]?.map(
+            (channel: Department | Category | Vendor): LeftTab => ({
+              name: channel.name,
+              location: {
+                pathname: `/${key}/${channel.id}`,
+                search: `?route=${MainGroups.Feeds}`,
+              },
+              icon: null,
+              subscription: {
+                type: key as keyof Subscription,
+                item: channel,
+              },
+              removable: true,
+              strict: true,
+            }),
+          ) || []),
+        ],
+        [],
+      );
+      menu[0].tabs.push(...subscriptionMenuItems);
+    }
     return menu;
   },
 });
