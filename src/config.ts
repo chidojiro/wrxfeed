c5b28afb7e679aee2c3a26a6023bb7bf285f4a7f
TaiPham <taipham1803@gmail.com>
2021-12-30 01:13:47 +0700

    chore: add key enable show user department

--- src/config.ts
+++ src/config.ts
@@ -13,3 +13,4 @@ export const EMAIL_SUPPORT_ADDRESS = 'support@gravitylabs.co';
 export const PUSHER_APP_KEY = process.env.PUSHER_APP_KEY || '';
 export const PUSHER_APP_CLUSTER = process.env.PUSHER_APP_CLUSTER || '';
 export const ENABLE_SUBSCRIPTION_SIDE_BAR = false;
+export const AUTO_ADD_USER_DEPARTMENT_SIDE_BAR = true;
