787702c35068805a825b5e3a3d8cc65156be636e
TaiPham <taipham1803@gmail.com>
2021-12-31 01:41:18 +0700

    chore: remove comment

--- src/main/states/target.state.ts
+++ src/main/states/target.state.ts
@@ -1,14 +1,6 @@
 import { atom, selector } from 'recoil';
 import { Target } from '@main/entity';
 
-// const initFilter = {
-//   offset: 0,
-//   limit: 30,
-//   year: new Date().getFullYear(),
-//   month: new Date().getMonth() + 1,
-//   timestamp: Date.now(),
-// };
-
 export const targetState = atom<Target[]>({
   key: 'main/target',
   default: selector({
