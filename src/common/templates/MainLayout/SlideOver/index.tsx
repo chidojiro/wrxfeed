1487c6397bf76c21990b0138ec35ff9c7ad540e1
TaiPham <taipham1803@gmail.com>
2022-01-04 09:47:50 +0700

    update: ease in/out transition duration to 200 for mobile and 300 for web (> sm)

--- src/common/templates/MainLayout/SlideOver/index.tsx
+++ src/common/templates/MainLayout/SlideOver/index.tsx
@@ -79,10 +79,10 @@ const SlideOver: React.VFC<SlideOverProps> = ({ className = '' }) => {
           <div className="fixed inset-y-0 right-0 max-w-md flex">
             <Transition.Child
               as={Fragment}
-              enter="transform transition ease-in-out duration-500 sm:duration-700"
+              enter="transform transition ease-in-out duration-200 sm:duration-300"
               enterFrom="translate-x-full"
               enterTo="translate-x-0"
-              leave="transform transition ease-in-out duration-500 sm:duration-700"
+              leave="transform transition ease-in-out duration-200 sm:duration-300"
               leaveFrom="translate-x-0"
               leaveTo="translate-x-full"
             >
