d2b6569fa94f0645c2e965e0357462640d8e2391
TaiPham <taipham1803@gmail.com>
2022-01-12 10:01:09 +0700

    fix: reduce zindex in line item to not cover the mention popover

--- src/common/templates/MainLayout/SlideOver/index.tsx
+++ src/common/templates/MainLayout/SlideOver/index.tsx
@@ -86,14 +86,14 @@ const SlideOver: React.VFC<SlideOverProps> = ({ className = '' }) => {
     if (item?.id && open && !loading) {
       getLineItemDetails(item?.id);
     }
-  }, [item?.id]);
+  }, [item]);
 
   return (
     <Transition.Root show={open} as={Fragment}>
       <div className={classNames('absolute inset-0 overflow-hidden', className)}>
         {/* <Dialog.Overlay className="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity" /> */}
-        <div role="alertdialog" className="absolute inset-0 z-10" onClick={onClickOverlay} />
-        <div className="fixed inset-y-0 z-30 right-0 max-w-md flex">
+        <div role="alertdialog" className="absolute inset-0 z-5" onClick={onClickOverlay} />
+        <div className="fixed inset-y-0 z-20 right-0 max-w-md flex">
           <Transition.Child
             as={Fragment}
             enter="transform transition ease-in-out duration-200 sm:duration-300"
@@ -103,7 +103,7 @@ const SlideOver: React.VFC<SlideOverProps> = ({ className = '' }) => {
             leaveFrom="translate-x-0"
             leaveTo="translate-x-full"
           >
-            <div className="w-screen max-w-md flex flex-1 flex-col pt-navbar relative z-30">
+            <div className="w-screen max-w-md flex flex-1 flex-col pt-navbar relative">
               {!!item && (
                 <LineItemDetails
                   open={open}
