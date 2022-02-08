07bf4bfefabb750c24b3046fe38404f9377b2b8b
TaiPham <taipham1803@gmail.com>
2022-02-08 12:40:25 +0700

    feat: lock add target modal to the top

--- src/common/atoms/Modal/index.tsx
+++ src/common/atoms/Modal/index.tsx
@@ -1,14 +1,24 @@
 import React, { Fragment, MutableRefObject, ReactElement } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
+import { classNames } from '@common/utils';
 
 export interface ModalProps {
   children?: Element | ReactElement | null;
   initialFocus?: MutableRefObject<HTMLElement | null>;
   open: boolean;
   onClose: () => void;
+  center?: boolean;
+  contentClass?: string;
 }
 
-const Modal: React.FC<ModalProps> = ({ children, initialFocus, open, onClose }) => {
+const Modal: React.FC<ModalProps> = ({
+  children,
+  initialFocus,
+  open,
+  onClose,
+  center = true,
+  contentClass = '',
+}) => {
   return (
     <Transition.Root show={open} as={Fragment}>
       <Dialog
@@ -29,9 +39,11 @@ const Modal: React.FC<ModalProps> = ({ children, initialFocus, open, onClose }) 
             <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-50 transition-opacity" />
           </Transition.Child>
           {/* This element is to trick the browser into centering the modal contents. */}
-          <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
-            &#8203;
-          </span>
+          {center && (
+            <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+              &#8203;
+            </span>
+          )}
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -41,7 +53,12 @@ const Modal: React.FC<ModalProps> = ({ children, initialFocus, open, onClose }) 
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <div className="inline-block align-bottom bg-white rounded-sm text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle">
+            <div
+              className={classNames(
+                'inline-block bg-white rounded-sm text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle',
+                contentClass,
+              )}
+            >
               {children}
             </div>
           </Transition.Child>
