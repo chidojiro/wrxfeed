f565d3c65a38e1a292f839de17f6f08872ac62a2
taipham <taipham1803@gmail.com>
2022-02-24 12:46:43 +0700

    feat: add rechart lib

--- src/main/molecules/TargetChartView/index.tsx
+++ src/main/molecules/TargetChartView/index.tsx
@@ -1,6 +1,15 @@
 import { classNames } from '@common/utils';
 import React from 'react';
 import { BasicsEditCircle } from '@assets/index';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts';
 
 interface TargetChartViewProps {
   className?: string;
@@ -14,6 +23,51 @@ export type SymbolNote = {
 };
 
 export const targetChartData = [];
+
+const data = [
+  {
+    name: 'Feb 4',
+    dec: 0,
+    jan: 0,
+    feb: 0,
+  },
+  {
+    name: 'Feb 8',
+    dec: 260,
+    jan: 200,
+    feb: 300,
+  },
+  {
+    name: 'Feb 12',
+    dec: 620,
+    jan: 400,
+    feb: 600,
+  },
+  {
+    name: 'Feb 16',
+    dec: 1670,
+    jan: 780,
+    feb: 1520,
+  },
+  {
+    name: 'Feb 20',
+    dec: 2181,
+    jan: 1890,
+    feb: 3218,
+  },
+  {
+    name: 'Feb 24',
+    dec: 2500,
+    jan: 2390,
+    feb: 3800,
+  },
+  {
+    name: 'Feb 28',
+    dec: 2800,
+    jan: 3490,
+    feb: 5300,
+  },
+];
 
 const TargetChartView: React.VFC<TargetChartViewProps> = ({ className }) => {
   const onClickEditTarget = () => undefined;
@@ -67,8 +121,36 @@ const TargetChartView: React.VFC<TargetChartViewProps> = ({ className }) => {
   const renderChartVisualize = () => {
     return (
       <div className="flex flex-col mt-2 w-full h-[479px]">
-        <div className="flex flex-1 justify-center items-center bg-blue-100">
-          <p className="text-3xl text-Gray-6">Target Charts</p>
+        <div className="flex flex-1 justify-center items-center py-6">
+          <ResponsiveContainer width="100%" height="100%">
+            <LineChart
+              width={500}
+              height={300}
+              data={data}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              {/* <Legend /> */}
+              <Line name="Dec" type="linear" dataKey="dec" strokeWidth={4} stroke="#E7E8EC" />
+              <Line name="Jan" type="linear" dataKey="jan" strokeWidth={4} stroke="#C5C8CD" />
+              <Line
+                name="Feb"
+                type="linear"
+                dataKey="feb"
+                strokeWidth={4}
+                stroke="#6565FB"
+                activeDot={{ r: 8 }}
+              />
+            </LineChart>
+          </ResponsiveContainer>
         </div>
         {renderChartSymbolNote()}
       </div>
@@ -89,11 +171,11 @@ const TargetChartView: React.VFC<TargetChartViewProps> = ({ className }) => {
   return (
     <div className={classNames('flex flex-col mt-4 w-full px-8', className ?? '')}>
       <div className="flex flex-row space-x-4 w-auto">
-        <div className="flex flex-col">
+        <div className="flex flex-col min-w-[128px]">
           <p className="text-xs text-Gray-3">Current Spend</p>
           <p className="text-xl text-primary font-bold mt-1">$60,400.76</p>
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col min-w-[128px]">
           <p className="text-xs text-Gray-3">Target</p>
           <p className="text-xl text-primary font-bold mt-1">$75,000</p>
         </div>
