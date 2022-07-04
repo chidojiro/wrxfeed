accaef2ef0a206dd9f6036091a07ac06b3a10ee3
Nick <chidojiro@gmail.com>
2022-07-04 10:06:08 +0700

    feat: infinite loader

---
+++ src/common/components/InfiniteLoader/InfiniteLoader.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useInfiniteLoader, UseInfiniteLoaderProps } from '@/common/hooks';
+import ListLoading from '@/main/atoms/ListLoading';
+import { ClassName } from '@/common/types';
+import clsx from 'clsx';
+
+type InfiniteLoaderProps<T = unknown> = ClassName &
+  Omit<UseInfiniteLoaderProps<T>, 'anchor'> & {
+    //
+  };
+
+export const InfiniteLoader = <T,>({ className, ...restProps }: InfiniteLoaderProps<T>) => {
+  const ref = React.useRef<HTMLDivElement>(null);
+  const { isExhausted } = useInfiniteLoader({ ...restProps, anchor: ref.current });
+
+  const renderContent = () => {
+    if (isExhausted)
+      return (
+        <p
+          className={clsx(
+            'text-base text-center text-Neutral-4 mt-3 sm:mt-8 inline-block',
+            className,
+          )}
+        >
+          <span>That&apos;s all for now.</span>
+          <span role="img" aria-label="rocket">
+            {' '}
+            🚀
+          </span>
+        </p>
+      );
+
+    return <ListLoading ref={ref} className={clsx('inline-block', className)} />;
+  };
+
+  return <div className="flex items-center justify-center mt-6">{renderContent()}</div>;
+};
---
+++ src/common/hooks/useInfiniteLoader.ts
@@ -0,0 +1,72 @@
+import { AssertUtils } from './../utils/assert';
+import { useDisclosure } from '@dwarvesf/react-hooks';
+import React from 'react';
+import { useFetcher } from './useFetcher';
+
+export type UseInfiniteLoaderProps<T = unknown> = {
+  onLoad: (page: number) => Promise<T>;
+  until: (data: T) => boolean;
+  onSuccess?: (data: T) => void;
+  onError?: (error: any) => void;
+  anchor: React.RefObject<HTMLElement> | HTMLElement | null;
+};
+
+export type UseInfiniteLoaderReturn = {
+  isExhausted: boolean;
+  isLoading: boolean;
+};
+
+export const useInfiniteLoader = <T = unknown>({
+  onLoad,
+  until,
+  onError,
+  onSuccess,
+  anchor,
+}: UseInfiniteLoaderProps<T>): UseInfiniteLoaderReturn => {
+  const [page, setPage] = React.useState(1);
+
+  const exhaustDisclosure = useDisclosure();
+  const readyForNextLoadDisclosure = useDisclosure();
+
+  const { isInitializing: isLoading } = useFetcher(['infiniteLoader', page], () => onLoad(page), {
+    onSuccess: (data) => {
+      onSuccess?.(data);
+
+      if (until(data)) {
+        exhaustDisclosure.onOpen();
+      } else {
+        exhaustDisclosure.onClose();
+      }
+    },
+    onError,
+  });
+
+  React.useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        console.log('hellop');
+        const isIntersecting = entries[0].isIntersecting;
+        console.log(isIntersecting);
+        if (isIntersecting) {
+          setPage((prev) => prev + 1);
+        }
+      },
+      {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.3,
+      },
+    );
+
+    const anchorNode = AssertUtils.isRef(anchor) ? anchor.current : anchor;
+
+    if (anchorNode) {
+      observer.observe(anchorNode);
+    }
+  }, [anchor]);
+
+  return React.useMemo(
+    () => ({ isLoading, isExhausted: exhaustDisclosure.isOpen }),
+    [exhaustDisclosure.isOpen, isLoading],
+  );
+};
