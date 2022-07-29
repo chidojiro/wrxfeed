19deeb53df8c938b011adbd8dbd897c2395d1be6
taipham <taipham1803@gmail.com>
2022-07-29 17:13:47 +0700

    chore: remove tooltip for target feed item name

--- src/feed/FeedCard/TargetFeedCard.tsx
+++ src/feed/FeedCard/TargetFeedCard.tsx
@@ -59,20 +59,6 @@ export const TargetFeedCard = React.memo(
           <p className="text-base text-primary text-left font-bold line-clamp-2 overflow-ellipsis">
             {getTargetName(target)}
           </p>
-          <div className="invisible group-hover:visible absolute bottom-8 left-0">
-            <div className="bg-primary p-2 rounded-sm">
-              <p className="text text-white text-2xs font-semibold">{getTargetName(target)}</p>
-            </div>
-            <svg
-              className="absolute text-primary h-2 right-8 top-full"
-              x="0px"
-              y="0px"
-              viewBox="0 0 255 255"
-              xmlSpace="preserve"
-            >
-              <polygon className="fill-current" points="0,0 127.5,127.5 255,0" />
-            </svg>
-          </div>
         </div>
       );
     };
