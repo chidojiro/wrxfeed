a9aa3a6b0cb76f46596ebba17057ce8b867b9219
taipham <taipham1803@gmail.com>
2022-08-05 17:10:34 +0700

    fix: tooltip style

--- src/common/components/Tooltip/Tooltip.tsx
+++ src/common/components/Tooltip/Tooltip.tsx
@@ -44,10 +44,10 @@ export const Tooltip = ({
     >
       <div>
         <div className="flex flex-col items-center">
-          <span className="z-10 p-2 text-xs leading-none text-white whitespace-no-wrap bg-gray-600 shadow-lg rounded">
+          <span className="z-10 p-2 text-2xs leading-none text-white whitespace-no-wrap bg-primary shadow-lg rounded-sm">
             {children}
           </span>
-          <div className={clsx('w-3 h-3 -mt-2 rotate-45 bg-gray-600', arrowClassName)} />
+          <div className={clsx('w-3 h-3 -mt-2 rotate-45 bg-primary', arrowClassName)} />
         </div>
       </div>
     </Popover>
