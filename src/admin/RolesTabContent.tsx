0bf6c698676aa1b2cb46686d5af56d09e9239da2
Nam Tran <namtrhg@gmail.com>
2022-09-07 11:14:18 +0700

    fix: update

--- src/admin/RolesTabContent.tsx
+++ src/admin/RolesTabContent.tsx
@@ -1,3 +1,13 @@
 export const RolesTabContent = () => {
-  return <div>Roles Tab Content</div>;
+  return (
+    <div className="flex flex-col">
+      <div className="space-y-2">
+        <h1 className="text-2xl leading-7 font-semibold text-primary">Roles</h1>
+        <p className="text-sm leading-4 font-normal text-Gray-6">
+          Manage account roles and view access.
+        </p>
+      </div>
+      <div>Roles Tab Content</div>
+    </div>
+  );
 };
