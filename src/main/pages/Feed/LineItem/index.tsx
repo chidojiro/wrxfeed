5702a59ac55347a8285bbce1373f1bcbaabfab21
Nam Tran <namtrhg@gmail.com>
2022-10-13 18:10:36 +0700

    fix: use empty array

--- src/main/pages/Feed/LineItem/index.tsx
+++ src/main/pages/Feed/LineItem/index.tsx
@@ -1,21 +1,21 @@
+import { EMPTY_ARRAY } from '@/common/constants';
 import { useFetcher } from '@/common/hooks';
 import { FeedApis } from '@/feed/apis';
 import { TransactionFeedItemCard } from '@/feed/FeedCard/TransactionFeedItemCard';
 import { MainLayout } from '@/layout/MainLayout';
-import { FeedItem } from '@/main/entity';
 import { useParams } from 'react-router-dom';
 
 export const LineItemPage = () => {
   const params = useParams<{ id: string }>();
   const id = Number(params.id);
 
-  const { data: feed, isInitializing } = useFetcher(['feedLineItems', params], () =>
+  const { data: feed = EMPTY_ARRAY, isInitializing } = useFetcher(['feedLineItems', params], () =>
     FeedApis.getTransactionFeedItem(id),
   );
 
   return (
     <MainLayout>
-      <TransactionFeedItemCard id={id} feed={feed?.[0] as FeedItem} loading={isInitializing} />
+      <TransactionFeedItemCard id={id} feed={feed?.[0]} loading={isInitializing} />
     </MainLayout>
   );
 };
