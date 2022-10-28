3c0bf5917a9f7b43cbc6d3efd0010e967eb0298e
Nam Tran <namtrhg@gmail.com>
2022-10-28 17:01:21 +0700

    fix: add restricted item to spending chart

--- src/spending/GroupedSpendingChartLegends.tsx
+++ src/spending/GroupedSpendingChartLegends.tsx
@@ -1,9 +1,12 @@
+import { RestrictedItem } from '@/auth/RestrictedItem';
 import { Button } from '@/common/components';
 import { ClassName } from '@/common/types';
-import { DateRangeFilter } from '@/feed/types';
 import { getDisplayUsdAmount } from '@/main/utils';
+import { RestrictedItem as TRestrictedItem } from '@/role/types';
+import { useRestrictedItems } from '@/role/useRestrictedItems';
 import { Entities } from '@/types';
 import clsx from 'clsx';
+import { isEqual } from 'lodash-es';
 import { Spending } from './types';
 import { getThisYearTotalsGroupedByItem } from './utils';
 
@@ -31,28 +34,40 @@ export const GroupedSpendingChartLegends = ({
 }: GroupedSpendingChartLegendsProps) => {
   const thisYearTotals = getThisYearTotalsGroupedByItem(spendings);
 
+  const { restrictedItems } = useRestrictedItems();
+
+  const isRestricted = (id: number, type: TRestrictedItem['type']) => {
+    return !!restrictedItems.find((item) => isEqual({ id, type }, item));
+  };
+
   return (
     <div className={clsx('flex flex-col', className)}>
       <h3 className="font-semibold h-10 flex items-end">{LabelsByGroupBy[groupBy]}</h3>
       <div className="border border-Gray-12 rounded-lg p-2 min-w-[180px] mt-3 text-[10px] flex flex-col flex-1 overflow-auto max-h-[400px]">
         {thisYearTotals.flat().map(({ color, id, name, total }) => (
-          <Button
-            key={id}
-            className={clsx('flex items-center justify-between gap-2 py-1', {
-              'opacity-30': highlightedItemId && highlightedItemId !== id,
-            })}
-            onMouseEnter={() => onItemMouseEnter?.(id)}
-            onMouseLeave={onItemMouseLeave}
-          >
-            <div className="flex items-center gap-2">
-              <div
-                className="w-1.5 h-1.5 rounded-full flex-shrink-0"
-                style={{ background: color }}
-              ></div>
-              <span className="break-words">{name}</span>
-            </div>
-            <span className="text-Gray-3">{getDisplayUsdAmount(total)}</span>
-          </Button>
+          <>
+            {isRestricted(id ?? 0, groupBy) ? (
+              <RestrictedItem />
+            ) : (
+              <Button
+                key={id}
+                className={clsx('flex items-center justify-between gap-2 py-1', {
+                  'opacity-30': highlightedItemId && highlightedItemId !== id,
+                })}
+                onMouseEnter={() => onItemMouseEnter?.(id)}
+                onMouseLeave={onItemMouseLeave}
+              >
+                <div className="flex items-center gap-2">
+                  <div
+                    className="w-1.5 h-1.5 rounded-full flex-shrink-0"
+                    style={{ background: color }}
+                  ></div>
+                  <span className="break-words">{name}</span>
+                </div>
+                <span className="text-Gray-3">{getDisplayUsdAmount(total)}</span>
+              </Button>
+            )}
+          </>
         ))}
       </div>
     </div>
