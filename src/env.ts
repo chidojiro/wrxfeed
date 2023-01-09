f642ff1fb3c6387e04d7fb6c7fa3c406298dd38e
Nick <chidojiro@gmail.com>
2023-01-09 10:55:39 +0700

    chore: environment variables elaboration

--- src/env.ts
+++ src/env.ts
@@ -1,11 +1,28 @@
+/**
+ * Node compilation environment:
+ * - development: when developing the app
+ * - production: when build the app with optimizations
+ */
 export const NODE_ENV = process.env.NODE_ENV ?? 'development';
 
+/**
+ * Running app environment: dev | staging | production | demo
+ */
 export const BUILD_ENV = process.env.REACT_APP_BUILD_ENV ?? 'dev';
 
+/**
+ * Backend api base endpoint
+ */
 export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL ?? '/api';
 
+/**
+ * Mix panel token
+ */
 export const MIX_PANEL_PROJECT_TOKEN = process.env.REACT_APP_MIX_PANEL_PROJECT_TOKEN ?? '';
 
+/**
+ * Pusher credentials
+ */
 export const PUSHER_APP_KEY = process.env.REACT_APP_PUSHER_APP_KEY ?? '';
 export const PUSHER_APP_CLUSTER = process.env.REACT_APP_PUSHER_APP_CLUSTER ?? '';
 
