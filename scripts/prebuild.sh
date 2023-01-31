da7a1962133c6dde166a171a2845590482e7872d
Nick <chidojiro@gmail.com>
2023-01-31 23:19:48 +0700

    feat: commit build

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -1,1 +1,1 @@
-sed -i  -e s/%BUILD_VERSION%/b2caedd59bf8d072ca944e956ccfc887a2a07b98/g scripts/../public/index.html
+sed -i  -e s/%BUILD_VERSION%/c1247ebd31ea9348c35406e451ab1287b3e34c69/g scripts/../public/index.html
