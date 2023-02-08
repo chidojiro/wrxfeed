342cfccfc64d1bd4cd165132b8b55f221cc947eb
Nam Tran <namtrhg@gmail.com>
2023-02-08 13:29:23 +0700

    fix: update sidebar

--- src/layout/NavBarStatic.tsx
+++ src/layout/NavBarStatic.tsx
@@ -1,9 +1,8 @@
-import { AddSmallIcon, FeedIcon, TargetIcon, TeamIcon, UserPlusIcon } from '@/assets';
+import { AddSmallIcon, ChartIcon, FeedIcon, TargetIcon, TeamIcon, UserPlusIcon } from '@/assets';
 import { ProtectedFeatures } from '@/auth/constants';
 import { Avatar, Button } from '@/common/components';
 import { NotifyPopover, UserProfilePopover } from '@/main/molecules';
 import { InviteModal } from '@/main/organisms';
-import { identifyMixPanelUserProfile } from '@/mixpanel/useMixPanel';
 import { useProfile } from '@/profile/useProfile';
 import { Routes } from '@/routing/routes';
 import { useSubscription } from '@/subscription/useSubscription';
@@ -15,7 +14,6 @@ import { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import SearchBar from './SearchBar';
 import { SidebarAccordion } from './SideBar/SidebarAccordion';
-import { SidebarNavGroup } from './SideBar/SidebarNavGroup';
 import { SidebarNavItem } from './SideBar/SidebarNavItem';
 import { UnfollowButton } from './SideBar/UnfollowButton';
 
@@ -49,7 +47,9 @@ export const NavBarStatic = ({
 
   const { departments } = subscription ?? {};
 
-  const { profile } = useProfile();
+  const { profile, isPermittedToFeature } = useProfile();
+
+  const showInvite = isPermittedToFeature(ProtectedFeatures.Invite);
 
   const onClickNotification = () => {
     history.push(Routes?.Notifications?.path as string);
@@ -60,9 +60,8 @@ export const NavBarStatic = ({
     mixpanel.track('Invite Button Click', {
       user_id: profile?.id,
       email: profile?.email,
-      company_id: profile?.company?.id,
+      company: profile?.company?.id,
     });
-    identifyMixPanelUserProfile(profile);
   };
 
   const renderInviteButton = () => {
@@ -99,7 +98,7 @@ export const NavBarStatic = ({
             <div className="relative flex flex-row-reverse px-2 sm:px-0 justify-between h-navbar md:grid md:grid-cols-12">
               <div className="flex flex-col justify-center md:col-span-3 pl-2 sm:pl-12 pr-3">
                 <h1 className={clsx('text-lg font-bold text-white', companyStyle)}>
-                  <Link to="/dashboard/all-company">{companyName}</Link>
+                  {companyName}
                 </h1>
               </div>
               <div className="hidden sm:flex items-center min-w-0 flex-1 md:pl-6 md:col-span-6 lg:col-span-6">
@@ -114,7 +113,7 @@ export const NavBarStatic = ({
                 {renderInviteButton()}
               </div>
               <div className="flex items-center md:absolute md:right-0 md:inset-y-0 sm:hidden">
-                <Popover.Button className="-mx-2 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-Accent-2">
+                <Popover.Button className="-mx-2 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-rose-500">
                   <span className="sr-only">Open menu</span>
                   {open ? (
                     <XIcon className="block h-6 w-6" aria-hidden="true" />
@@ -143,7 +142,7 @@ export const NavBarStatic = ({
                   </div>
                   <Button
                     onClick={onClickNotification}
-                    className="ml-auto flex-shrink-0 bg-white rounded-full p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-Accent-2"
+                    className="ml-auto flex-shrink-0 bg-white rounded-full p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500"
                   >
                     <span className="sr-only">View notifications</span>
                     <BellIcon className="h-6 w-6" aria-hidden="true" />
@@ -151,18 +150,6 @@ export const NavBarStatic = ({
                 </div>
                 <div className="mt-3 max-w-3xl mx-auto px-2 space-y-1 sm:px-4">
                   <div className="block rounded-md py-4 px-6 text-base font-medium text-gray-500 hover:bg-gray-50 hover:text-gray-900">
-                    <SidebarNavGroup className="!p-0">Boards</SidebarNavGroup>
-                    <SidebarNavItem
-                      className="!p-0"
-                      href="/dashboard/all-company"
-                      iconLeft={<TargetIcon />}
-                      matches={['/dashboard/:slug']}
-                    >
-                      Targets
-                    </SidebarNavItem>
-                  </div>
-                  <div className="block rounded-md py-4 px-6 text-base font-medium text-gray-500 hover:bg-gray-50 hover:text-gray-900">
-                    <SidebarNavGroup className="!p-0">Following</SidebarNavGroup>
                     <SidebarAccordion
                       defaultOpen={false}
                       label="Teams"
@@ -198,12 +185,23 @@ export const NavBarStatic = ({
                     </SidebarAccordion>
                   </div>
                   <div className="block rounded-md py-4 px-6 text-base font-medium text-gray-500 hover:bg-gray-50 hover:text-gray-900">
-                    <SidebarNavGroup className="!p-0">Feeds</SidebarNavGroup>
+                    <SidebarNavItem
+                      className="!p-0"
+                      href="/dashboard/all-company"
+                      iconLeft={<TargetIcon />}
+                      matches={['/dashboard/:slug']}
+                    >
+                      All Company
+                    </SidebarNavItem>
+                  </div>
+                  <div className="block rounded-md py-4 px-6 text-base font-medium text-gray-500 hover:bg-gray-50 hover:text-gray-900">
+                    <SidebarNavItem className="!p-0" href="/insights" iconLeft={<ChartIcon />}>
+                      Insights
+                    </SidebarNavItem>
+                  </div>
+                  <div className="block rounded-md py-4 px-6 text-base font-medium text-gray-500 hover:bg-gray-50 hover:text-gray-900">
                     <SidebarNavItem className="!p-0" href="/feeds/for-you" iconLeft={<FeedIcon />}>
                       For you
-                    </SidebarNavItem>
-                    <SidebarNavItem className="!p-0" href="/feeds/company" iconLeft={<FeedIcon />}>
-                      Company
                     </SidebarNavItem>
                   </div>
                 </div>
@@ -216,7 +214,9 @@ export const NavBarStatic = ({
               Sign out
             </a>
           </Popover.Panel>
-          <InviteModal open={isOpenInviteModal} onClose={() => openInviteModal(false)} />
+          {showInvite && (
+            <InviteModal open={isOpenInviteModal} onClose={() => openInviteModal(false)} />
+          )}
         </>
       )}
     </Popover>
