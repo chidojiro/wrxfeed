09be694fe789304d4efc80cb0531ac36264e865b
Nick <chidojiro@gmail.com>
2023-03-01 17:23:17 +0700

    fix: clean up analyze config

--- config-overrides.js
+++ config-overrides.js
@@ -1,26 +1,21 @@
 const path = require('path');
-const { override } = require('customize-cra');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
-module.exports = override(
-  (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname, 'src'),
-    };
+module.exports = function override(config) {
+  config.resolve.alias = {
+    ...config.resolve.alias,
+    '@': path.resolve(__dirname, 'src'),
+  };
 
-    return config;
-  },
-  (config) => {
-    if (process.env.ANALYZE) {
-      config.plugins.push(
-        new BundleAnalyzerPlugin({
-          analyzerMode: 'static',
-          reportFilename: 'bundle-report.html',
-          openAnalyzer: true,
-        }),
-      );
-    }
-    return config;
-  },
-);
+  if (process.env.ANALYZE) {
+    config.plugins.push(
+      new BundleAnalyzerPlugin({
+        analyzerMode: 'static',
+        reportFilename: 'bundle-report.html',
+        openAnalyzer: true,
+      }),
+    );
+  }
+
+  return config;
+};
